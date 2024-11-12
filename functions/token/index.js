96abc961b81ab91e9865c5663c2f8b4181fc5550
Brendan Dalpe <bdalpe@gmail.com>
2024-11-12 13:50:07 -0600

    Add weighted and integer generators

--- functions/token/index.js
+++ functions/token/index.js
@@ -13,17 +13,44 @@
  *  - value1
  *  - value2
  *  - value3
+ *
+ * or:
+ *
+ * token: tok
+ * weightedPick:
+ *  - value: value1
+ *    weight: 1
+ *  - value: value2
+ *    weight: 2
+ *  - value: value3
+ *    weight: 3
+ *
+ * or:
+ *
+ * token: tok
+ * int:
+ *   min: 1
+ *   max: 10
  * ```
  */
 
 let config = {};
+let next = randomPick();
 
 exports.init = (cfg) => {
     config = cfg;
+
+    if ('weightedPick' in config) {
+        next = cumulativeWeightedRandomPick();
+    }
+
+    if ('int' in config) {
+        next = randomIntegerPick();
+    }
 }
 
 exports.process = (event) => {
-    event.event = event.event.replaceAll(`{{${config.token}}}`, randomPick().next().value);
+    event.event = event.event.replaceAll(`{{${config.token}}}`, next.next().value);
 
     return event;
 }
@@ -31,5 +58,36 @@ exports.process = (event) => {
 function* randomPick() {
     while (true) {
         yield config.pickFrom[Math.floor(Math.random() * config.pickFrom.length)];
+    }
+}
+
+function* cumulativeWeightedRandomPick() {
+    const items = config.weightedPick;
+    const cumulativeWeights = [];
+    let totalWeight = 0;
+
+    // Build the cumulative distribution array
+    for (const item of items) {
+        totalWeight += item.weight || 1; // Default weight to 1 if not provided
+        cumulativeWeights.push({ value: item.value, cumulative: totalWeight });
+    }
+
+    // Infinite loop to yield values based on the cumulative distribution
+    while (true) {
+        const randomValue = Math.random() * totalWeight;
+
+        // Use the cumulative array to pick a value
+        for (const entry of cumulativeWeights) {
+            if (randomValue < entry.cumulative) {
+                yield entry.value;
+                break;
+            }
+        }
+    }
+}
+
+function* randomIntegerPick() {
+    while (true) {
+        yield Math.floor(Math.random() * (config.int.max - config.int.min + 1)) + config.int.min;
     }
 }
