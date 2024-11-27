ccfe545cd556d169f95a99129dda51273e9831eb
Brendan Dalpe <bdalpe@gmail.com>
2024-11-27 16:08:06 -0600

    Don't copy functions

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,18 +1,9 @@
 import { build } from "esbuild";
-import { copy } from 'esbuild-plugin-copy';
 
 build({
   entryPoints: ["src/index.ts"],
   bundle: true,
   platform: "node",
   minify: true,
-  outdir: "dist",
-  plugins: [
-    copy({
-      assets: {
-        from: 'src/functions/**/*.js',
-        to: 'functions',
-      }
-    }),
-  ],
+  outdir: "dist"
 });
